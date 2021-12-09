e5489c526326d1710810aeabdd67afa6217e138e
Patrick Paolella <pat@openfin.co>
2021-12-09 12:28:35 -0500

    Test 1

--- build/index.js
+++ build/index.js
@@ -1,85 +1,162 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.waitForFinDesktop = exports.checkFinGetVersion = exports.switchWindowByTitle = exports.switchWindow = exports.launcher = exports.default = void 0;
-
-require("source-map-support/register");
-
-var _launcher = _interopRequireDefault(require("./launcher"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-class ChromeService {}
-
-exports.default = ChromeService;
-const launcher = _launcher.default;
-exports.launcher = launcher;
-
-const switchWindow = async (windowHandle, callback) => {
-  await browser.switchToWindow(windowHandle);
-  const title = await browser.getTitle();
-  await callback(title);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-
-exports.switchWindow = switchWindow;
-
-const switchWindowByTitle = async windowTitle => {
-  const handles = await browser.getWindowHandles();
-  let handleIndex = 0;
-
-  let checkTitle = async title => {
-    console.log(title, windowTitle);
-
-    if (title !== windowTitle) {
-      handleIndex++;
-
-      if (handleIndex < handles.length) {
-        await switchWindow(handles[handleIndex], checkTitle);
-      } else {
-        // the window may not be loaded yet, so call itself again
-        await switchWindowByTitle(windowTitle);
-      }
-    } else {
-      console.log(`matched ${handleIndex}`, title, windowTitle);
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-  };
-
-  await switchWindow(handles[handleIndex], checkTitle);
 };
+exports.__esModule = true;
+exports.waitForFinDesktop = exports.checkFinGetVersion = exports.switchWindowByTitle = exports.switchWindow = exports.launcher = void 0;
+var launcher_1 = require("./launcher");
+var ChromeService = /** @class */ (function () {
+    function ChromeService() {
+    }
+    return ChromeService;
+}());
+exports["default"] = ChromeService;
+exports.launcher = launcher_1["default"];
+var switchWindow = function (windowHandle, callback) { return __awaiter(void 0, void 0, void 0, function () {
+    var title;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, browser.switchToWindow(windowHandle)];
+            case 1:
+                _a.sent();
+                return [4 /*yield*/, browser.getTitle()];
+            case 2:
+                title = _a.sent();
+                return [4 /*yield*/, callback(title)];
+            case 3:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); };
+exports.switchWindow = switchWindow;
+var switchWindowByTitle = function (windowTitle) { return __awaiter(void 0, void 0, void 0, function () {
+    var handles, handleIndex, checkTitle;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, browser.getWindowHandles()];
+            case 1:
+                handles = _a.sent();
+                handleIndex = 0;
+                checkTitle = function (title) { return __awaiter(void 0, void 0, void 0, function () {
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                console.log(title, windowTitle);
+                                if (!(title !== windowTitle)) return [3 /*break*/, 5];
+                                handleIndex++;
+                                if (!(handleIndex < handles.length)) return [3 /*break*/, 2];
+                                return [4 /*yield*/, exports.switchWindow(handles[handleIndex], checkTitle)];
+                            case 1:
+                                _a.sent();
+                                return [3 /*break*/, 4];
+                            case 2: 
+                            // the window may not be loaded yet, so call itself again
+                            return [4 /*yield*/, exports.switchWindowByTitle(windowTitle)];
+                            case 3:
+                                // the window may not be loaded yet, so call itself again
+                                _a.sent();
+                                _a.label = 4;
+                            case 4: return [3 /*break*/, 6];
+                            case 5:
+                                console.log("matched " + handleIndex, title, windowTitle);
+                                _a.label = 6;
+                            case 6: return [2 /*return*/];
+                        }
+                    });
+                }); };
+                return [4 /*yield*/, exports.switchWindow(handles[handleIndex], checkTitle)];
+            case 2:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); };
+exports.switchWindowByTitle = switchWindowByTitle;
 /**
  *  Check if OpenFin Javascript API fin.desktop.System.getVersion exits
  *
  **/
-
-
-exports.switchWindowByTitle = switchWindowByTitle;
-
-const checkFinGetVersion = async callback => {
-  const result = await browser.executeAsync(function (done) {
-    if (fin && fin.desktop && fin.desktop.System && fin.desktop.System.getVersion) {
-      done(true);
-    } else {
-      done(false);
-    }
-  });
-  callback(result);
-};
-
+var checkFinGetVersion = function (callback) { return __awaiter(void 0, void 0, void 0, function () {
+    var result;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, browser.executeAsync(function (done) {
+                    if (fin && fin.desktop && fin.desktop.System && fin.desktop.System.getVersion) {
+                        done(true);
+                    }
+                    else {
+                        done(false);
+                    }
+                })];
+            case 1:
+                result = _a.sent();
+                callback(result);
+                return [2 /*return*/];
+        }
+    });
+}); };
 exports.checkFinGetVersion = checkFinGetVersion;
-
-const waitForFinDesktop = async () => {
-  var callback = async ready => {
-    if (ready === true) {
-      return;
-    } else {
-      await browser.pause(1000);
-      await waitForFinDesktop();
-    }
-  };
-
-  await checkFinGetVersion(callback);
-};
-
+var waitForFinDesktop = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var callback;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                callback = function (ready) { return __awaiter(void 0, void 0, void 0, function () {
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                if (!(ready === true)) return [3 /*break*/, 1];
+                                return [2 /*return*/];
+                            case 1: return [4 /*yield*/, browser.pause(1000)];
+                            case 2:
+                                _a.sent();
+                                return [4 /*yield*/, exports.waitForFinDesktop()];
+                            case 3:
+                                _a.sent();
+                                _a.label = 4;
+                            case 4: return [2 /*return*/];
+                        }
+                    });
+                }); };
+                return [4 /*yield*/, exports.checkFinGetVersion(callback)];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); };
 exports.waitForFinDesktop = waitForFinDesktop;
